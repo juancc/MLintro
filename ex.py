ca344fb83df95b201975bb4af766ca2a5651011e
juancc <jkaz.os@gmail.com>
2023-07-19 11:48:21 -0500

    desarrollo ejercicio 12

---
+++ ex.py
@@ -0,0 +1,17 @@
+"""
+Ejercicios clase 1 ML
+
+JCA
+"""
+import numpy as np
+import cv2
+
+
+def linea(lado):
+    im = np.zeros( (lado, lado, 3) , np.uint8)
+    for i in range(lado): im[i,i] = [255,255,255]
+
+    cv2.imwrite('ex1.png', im)
+
+linea(100)
+    
